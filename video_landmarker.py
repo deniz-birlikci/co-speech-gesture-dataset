4a88d0fe65c8dfe4d234e6fa8fb7e5b1603c6d97
Mehmet Deniz Birlikci <mdbirlikci@gmail.com>
2023-06-27 18:45:41 -0400

    Increased quota

--- video_landmarker.py
+++ video_landmarker.py
@@ -243,7 +243,7 @@ if __name__ == "__main__":
     
     # run a pool with as many cpus as we have
     # cpu_count = multiprocessing.cpu_count()
-    cpu_count = 20
+    cpu_count = 24
     print("Working with {} cpus".format(cpu_count))
     with multiprocessing.Pool(processes=cpu_count) as pool:
         input("Continue? ")
