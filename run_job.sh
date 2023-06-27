a03c6cc4378513338031fcdda0967e8e4ad7b9a2
Mehmet Deniz Birlikci <mdbirlikci@gmail.com>
2023-06-27 17:24:12 -0400

    Sbatch job format

---
+++ run_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p cpu_long
+#SBATCH --pty
+#SBATCH --mem=94000
+
+# Load any required modules
+# module load python
+
+# Activate your virtual environment if necessary
+conda activate gesture
+
+# Change to the directory where your Python code is located
+# cd /path/to/your/python/code/directory
+
+# Run your Python code
+python video_landmarker.py
+
+# Deactivate the virtual environment if necessary
+conda deactivate
